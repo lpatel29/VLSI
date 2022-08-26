4802777118cf1395a9fdaee8655f53c6f3a86432
Linux Patel <84766312+lpatel29@users.noreply.github.com>
2022-08-26 11:20:40 +0530

    Create testbench.sv

---
+++ D_FLIP_FLOP/testbench.sv
@@ -0,0 +1,22 @@
+module mux4x1_tb();
+  reg a,b,c,d;
+  reg [1:0] sel;
+  wire out;
+  
+  mux4x1 dut(.a(a), .b(b), .c(c), .d(d), .sel(sel), .out(out));
+  initial begin
+    $dumpfile("mux4x1.vcd");
+    $dumpvars(1);
+  end
+  initial begin
+    sel=2'b00; a=0;b=1;c=0;d=1;
+    #5
+    sel=2'b01; a=0;b=1;c=0;d=1;
+    #5
+    sel=2'b10; a=0;b=1;c=0;d=1;
+    #5
+    sel=2'b11; a=0;b=1;c=0;d=1;
+    #5
+    $finish();
+  end
+endmodule
